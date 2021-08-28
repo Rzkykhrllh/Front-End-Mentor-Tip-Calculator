d1c8659bcc2274eb1fc83cf55cbc5e9dc367b4c5
Rzkykhrllh <Rizkykirigaya@gmail.com>
2021-08-28 15:14:40 +0800

    installing tailwind

---
+++ src/pages/Calculator.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Calculator() {
+    return (
+        <section className="">
+            <h1 className="bg-green-900 text-red-700">Hello World</h1>
+        </section>
+    )
+}
+
+export default Calculator
